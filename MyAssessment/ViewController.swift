25e8b5f4481c14f28bcec595cf04fead9be3305f
Tony Pham <tupv2@fsoft.com.vn>
2015-04-14 07:07:39 +0800

    Initial Commit

---
+++ MyAssessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAssessment
+//
+//  Created by Tony Pham on 4/14/15.
+//  Copyright (c) 2015 Tony Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
